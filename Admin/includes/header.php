ad8b04afe1c57780d00e4b345aa406ec3f8c833d
Neiltadz <neilgabrieltadea@gmail.com>
2021-05-24 13:15:19 +0800

    update

--- Admin/includes/header.php
+++ Admin/includes/header.php
@@ -3,19 +3,21 @@
 <head>
 	<title>Login</title>
 	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-	<link href="assets/css/style.css" rel="stylesheet">
-
+	<link href="assets/css/style.css" rel="stylesheet"> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
+</head>    
+    <section id="Header">
         <nav class="navbar navbar-expand-xl fixed-top navbar-dark bg-dark" >
             <div class="container-fluid">
-                <a class="navbar-brand" href="#header"><img src="img/adminlogo.png" style="margin-left: 60px;" width="default" height="60">
-                </a>
-                
-                <button class="navbar-toggler" data-toggle="collapse" data-target="NAVBAR" >
+                <a class="navbar-brand" href="#header"><img src="img/adminlogo.png" style="margin-left: 60px;" width="default" height="60"></a>
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#NAVBAR" >
                     <span class="navbar-toggler-icon"></span> 
                 </button>
       
     
-                <div class="collapse navbar-collapse" id="NAVBAR" >
+                <div class="collapse navbar-collapse " id="NAVBAR" >
 
                     <ul class="navbar-nav ms-auto text-center" style ="font-size:1.3em">
                     
@@ -43,4 +45,5 @@
 
             </div>
         </nav>
-</head>
+</section>
+<
