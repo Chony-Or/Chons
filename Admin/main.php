4c6f907a92a785d1f0f4c152b8e81a24b7ea37a4
aiavalles <aiavalles08@gmail.com>
2021-05-24 22:01:33 +0800

    main desc.

--- Admin/main.php
+++ Admin/main.php
@@ -16,10 +16,10 @@ img {
 </head>
 <body>
 
-<img id="mainlogo" src="img/logo.png" style="float: center; padding: 75px 9px 31px 9px;" width="20%"  >
+<img id="mainlogo" src="img/logo.png" style="float: center; padding: 98px 10px 31px 9px;" width="20%"  >
 
-<center><h1 style="color:black" >Hello!! Welcome to Lhoyzki Admin</h1></center>
-<center><p style="color:black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+<center><h1 style="color:black" >Welcome to Lhoyzki Admin</h1></center>
+<center><p style="color:black">This website was designed specifcally for the Admin users. Admin Users have the control and have.<br>
  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p></center>
 
