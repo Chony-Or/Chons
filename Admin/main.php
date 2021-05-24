80b3f19297e74f10a6ba3e26dfeb68475513fdd2
aiavalles <aiavalles08@gmail.com>
2021-05-24 15:48:47 +0800

    updated main

--- Admin/main.php
+++ Admin/main.php
@@ -18,7 +18,7 @@ img {
 
 <img id="mainlogo" src="img/logo.png" style="float: center; padding: 75px 9px 31px 9px;" width="20%"  >
 
-<center><h1 style="color:black" >Welcome to Lhoyzki Admin Interface</h1></center>
+<center><h1 style="color:black" >Hello!! Welcome to Lhoyzki Admin</h1></center>
 <center><p style="color:black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br>
   Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p></center>
