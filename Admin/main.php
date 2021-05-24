f46056f8532788c8e7c56f3e2ad3583f859f39e8
aiavalles <aiavalles08@gmail.com>
2021-05-24 11:12:59 +0800

    MAIN LOGO

--- Admin/main.php
+++ Admin/main.php
@@ -1,13 +1,13 @@
 <?php include "includes/db_connection.php" ?>
 <?php include "includes/header.php" ?>
-
-<body>
-echo '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<img src="/img/mainlogo.png" width="280" height="125" title="Logo of a group" alt="Logo of lhoyzski" />
+<body>
+<!DOCTYPE">
 
-</html>
-';
+<img src="/img/mainlogo.png" width="280" height="125" title="Logo" alt="Logo of lhoyzski" />
+
+
 </body>
+</html>
 
 <?php include "includes/footer.php" ?>
