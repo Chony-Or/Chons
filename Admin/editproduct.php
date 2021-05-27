2a111713de24e03c9d1aa68b1e8e88a8aed69bd9
Chony-Or <chonyor76@gmail.com>
2021-05-27 10:25:10 +0800

    up

--- Admin/editproduct.php
+++ Admin/editproduct.php
@@ -109,14 +109,14 @@ else
 
 <style>
 		.edit-form form{
-			color: #7a7a7a;
+	color: #7a7a7a;
 	border-radius: 50px;
-	width: 40%;
+	width:fit-content;
 	margin-bottom: 50px;
     background: #ececec;
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
     padding: 50px;	
-    position:absolute;	
+    position:relative;	
 		}
 		.edit-form {
 	height: 100%;
