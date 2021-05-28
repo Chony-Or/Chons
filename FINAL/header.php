2a5baf4ae39342f1f4203cb9427186df609dcedd
Neiltadz <neilgabrieltadea@gmail.com>
2021-05-28 17:52:24 +0800

    update receipt

--- FINAL/header.php
+++ FINAL/header.php
@@ -17,7 +17,7 @@
 <section id="header">
         <nav class="navbar navbar-expand-xl fixed-top navbar-light">
             <div class="container">
-            <a class="navbar-brand" href="#header"><img id="navbarBrand" src="img/coloredlogo.png" style="margin-left: 30px;" width="90" height="60"><span>Lhoyzki</span></a>
+            <a class="navbar-brand" href="HomePage.php"><img id="navbarBrand" src="img/coloredlogo.png" style="margin-left: 30px;" width="90" height="60"><span>Lhoyzki</span></a>
                 <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
                 <span class="navbar-toggler-icon"></span>
                 </button>
